50babf57510c10eaea170659a9ab55ad5c8121fc
Vitaliy Kovtun <vitaliy.k@active-bridge.com>
2017-11-06 14:41:28 +0200

    Fix code syntax issues

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,16 @@
 require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'action_cable/engine'
 # require "sprockets/railtie"
-require "rails/test_unit/railtie"
+require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,7 +18,7 @@ Bundler.require(*Rails.groups)
 
 module TwitterBot
   class Application < Rails::Application
-    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += %W[#{config.root}/lib]
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :post do
     gem_name { FFaker::Lorem.sentence }
-    from_version { FFaker::PhoneNumber.phone_number  }
-    to_version { FFaker::PhoneNumber.phone_number  }
+    from_version { FFaker::PhoneNumber.phone_number }
+    to_version { FFaker::PhoneNumber.phone_number }
   end
 
   after(:build) do |post|
