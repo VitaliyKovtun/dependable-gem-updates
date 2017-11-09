14285164218793b21221403bb03223af49d3c953
Vitaliy Kovtun <vitaliy.k@active-bridge.com>
2017-11-09 11:15:13 +0200

    Add gems versions dates

--- app/controllers/gems_controller.rb
+++ app/controllers/gems_controller.rb
@@ -7,6 +7,8 @@ class GemsController < ApplicationController
     @gem = Gems.info gem_name
     versions = Gems.versions gem_name
     @gem_versions = versions.map { |version| version['number'] }
+    newest_version = versions.find { |version| version['number'] == @gem['version'] }
+    @newest_version_date = newest_version['created_at'].to_date
   end
 
   helper_method :gems
